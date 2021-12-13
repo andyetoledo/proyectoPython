fe69c52410ffb6961de62cb6c2987c5183201d12
Andres Toledo <andres.toledo@uabc.edu.mx>
2021-12-12 21:03:41 -0800

    My first deploy 24

--- wcards/settings.py
+++ wcards/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'gestionTarjetas',
     'gestionUsuarios',
+    'gestionTarjetas',
 ]
 
 MIDDLEWARE = [
