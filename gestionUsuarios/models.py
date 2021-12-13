6f996ba548f4ea80c8e56bba4ddc2921c8a11f96
Andres Toledo <andres.toledo@uabc.edu.mx>
2021-12-12 19:29:16 -0800

    My first deploy 17

--- gestionUsuarios/models.py
+++ gestionUsuarios/models.py
@@ -3,8 +3,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class TipoUsuario(models.Model):
-    usuario_tipo = models.CharField(max_length=50)
-    idusuario = models.ManyToManyField(User, default=None)
+
 
 
