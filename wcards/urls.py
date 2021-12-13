4c7fb9c074746fab547dab966bf5dc2060586654
Andres Toledo <andres.toledo@uabc.edu.mx>
2021-12-12 21:49:51 -0800

    My first deploy 26

--- wcards/urls.py
+++ wcards/urls.py
@@ -17,12 +17,11 @@ from django.contrib import admin
 from django.urls import path
 
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 from gestionUsuarios.views import *
 from gestionTarjetas.views import *
-
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
